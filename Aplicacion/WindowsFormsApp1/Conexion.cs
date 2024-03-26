7d397391e7c1e9f70b729cdf27f057766d4bb4da
Roí Rosales <rosalesroy11@gmail.com>
2024-03-25 19:21:33 -0600

    Conexion Con base de datos
    Se realizo la conexion respectiva con la base de datos en mariadb

---
+++ Aplicacion/WindowsFormsApp1/Conexion.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class Conexion
+    {
+        MySqlConnection cnx = new MySqlConnection();
+        static string servidor = "localhost";
+        static string bd= "tarjetas";
+        static string usuario= "root";
+        static string password= "root123";
+        static string puerto= "3306";
+        string cadenaConexion = "server="+servidor+";"+"port="+puerto+";"+"user id="+usuario+";"+"password="+password+";"+"database="+bd+";";
+        public MySqlConnection establecerconexion() {
+            try {
+                cnx.ConnectionString = cadenaConexion;
+                cnx.Open();
+                MessageBox.Show("SE CONECTO CORRECTAMENTE");
+
+            }
+            catch (MySqlException e){
+                MessageBox.Show("ERROR: " + e.ToString());
+               
+            }
+            return cnx;
+        }
+
+    }
+}
